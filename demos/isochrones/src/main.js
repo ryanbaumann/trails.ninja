ba46bb11808aa461bc938eaf93664294b674544c
Ryan Baumann <rsbaumann@gmail.com>
2026-07-30 11:58:51 -0700

    chore: creds update

--- demos/isochrones/src/main.js
+++ demos/isochrones/src/main.js
@@ -439,7 +439,7 @@ async function initMap() {
     state.map = new Map(elements.map, {
       center: DEFAULT_CENTER,
       zoom: 10,
-      mapId: '556022f677234497',
+      mapId: '9e6b48a5b3653026f9d7556d',
       colorScheme: 'DARK',
       clickableIcons: false,
       mapTypeControl: false,
